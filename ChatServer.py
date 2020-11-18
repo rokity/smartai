132d9902ce29da5dc8f70a47c43db042d1b07f2c
riccardo amadio <riccardo.amadio@ammagamma.com>
2020-11-18 15:42:18 +0100

    chat server not complete

--- ChatServer.py
+++ ChatServer.py
@@ -1,5 +1,7 @@
 from telnetlib import Telnet
 import time
+import multiprocessing
+
 
 TIME = 0.3
 
@@ -11,12 +13,17 @@ class ChatServer:
     self.name= _name
     self.tn = Telnet(self.host, self.port)
     self.bootstrap_connection()
+    n_threads = multiprocessing.cpu_count()
+    print(n_threads)
+    self.pool = multiprocessing.Pool(processes=n_threads)
+    self.pool_results = multiprocessing.Manager().list()
 
   def bootstrap_connection(self):
     self.tn.write(("NAME "+self.name).encode('ascii') + b"\n")
 
   def join_existing_channel(self,channel):
     self.tn.write(("JOIN " + channel).encode('ascii') + b"\n")
+    self.pool.apply_async(func=run,args=(self,channel))
 
   def leave_existing_channel(self,channel):
     self.tn.write(("LEAVE " + channel ).encode('ascii') + b"\n")
@@ -29,3 +36,11 @@ class ChatServer:
 
   def is_connected(self):
       return False if self.tn.get_socket().fileno() == -1 else True
+  
+  def listen(self,channel):
+    while(True):
+      msg=self.tn.read_all()
+      print(msg)
+      
+
+
