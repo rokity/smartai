6d70a14de5862c8c3a4a7d71251b3cbb4ec0fcce
riccardo amadio <riccardo.amadio@ammagamma.com>
2020-12-01 13:15:07 +0100

    end chat server

--- ChatServer.py
+++ ChatServer.py
@@ -1,12 +1,14 @@
 from telnetlib import Telnet
 import time
 import multiprocessing
+import threading
 
 
 TIME = 0.3
 
 class ChatServer:
   channels = ["#GLOBAL","#CHAT","#LEAGUE","#LOGS","#DATA","#STREAM"]
+  channels_joined=[]
   messages=[]
   def __init__(self, _host, _port,_name):
     self.host = _host
@@ -18,9 +20,11 @@ class ChatServer:
 
   def bootstrap_connection(self):
     self.tn.write(("NAME "+self.name).encode('ascii') + b"\n")
+    threading.Thread(target=self.listen, args=()).start()
 
   def join_existing_channel(self,channel):
     self.tn.write(("JOIN " + channel).encode('ascii') + b"\n")
+    self.channels_joined.append(channel)
 
 
   def leave_existing_channel(self,channel):
@@ -34,6 +38,17 @@ class ChatServer:
 
   def is_connected(self):
       return False if self.tn.get_socket().fileno() == -1 else True
+      
+  def listen(self):
+    while(True):
+      message=self.tn.read_very_eager().decode("utf-8") 
+      if(len(message)>0):
+        message=message.split(" ")
+        if(message[0] in self.channels_joined):
+          message_text=" ".join(message[2:len(message)]).replace("\n", "")
+          message={"channel":message[0],"user":message[1],"message":message_text}
+          self.messages.append(message)
+          print(message)
   
 
       
