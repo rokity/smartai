31d7cdcf52a2a790bc47691392561a49581921ec
riccardo amadio <riccardo.amadio@ammagamma.com>
2020-12-01 12:46:08 +0100

    update chat server

--- ChatServer.py
+++ ChatServer.py
@@ -7,23 +7,21 @@ TIME = 0.3
 
 class ChatServer:
   channels = ["#GLOBAL","#CHAT","#LEAGUE","#LOGS","#DATA","#STREAM"]
+  messages=[]
   def __init__(self, _host, _port,_name):
     self.host = _host
     self.port = _port
     self.name= _name
     self.tn = Telnet(self.host, self.port)
     self.bootstrap_connection()
-    n_threads = multiprocessing.cpu_count()
-    print(n_threads)
-    self.pool = multiprocessing.Pool(processes=n_threads)
-    self.pool_results = multiprocessing.Manager().list()
+
 
   def bootstrap_connection(self):
     self.tn.write(("NAME "+self.name).encode('ascii') + b"\n")
 
   def join_existing_channel(self,channel):
     self.tn.write(("JOIN " + channel).encode('ascii') + b"\n")
-    self.pool.apply_async(func=run,args=(self,channel))
+
 
   def leave_existing_channel(self,channel):
     self.tn.write(("LEAVE " + channel ).encode('ascii') + b"\n")
@@ -37,10 +35,7 @@ class ChatServer:
   def is_connected(self):
       return False if self.tn.get_socket().fileno() == -1 else True
   
-  def listen(self,channel):
-    while(True):
-      msg=self.tn.read_all()
-      print(msg)
+
       
 
 
