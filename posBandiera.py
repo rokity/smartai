4073592702529fc82124719552914604307d2ba7
marcovaresi <74302073+marcovaresi@users.noreply.github.com>
2020-12-02 18:02:31 +0100

    Delete posBandiera.py

--- posBandiera.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-def pos_bandiera(mapp, symbol, size):
-	for i in range(0, size):
-		for j in range(0, size):
-			if (mapp[i][j] == 'X' and symbol == 0) or (mapp[i][j] == 'x' and symbol == 1):
-				return i, j
-
