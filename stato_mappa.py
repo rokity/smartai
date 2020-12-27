73ac14c390d5d7c69ecad42f560bacfd0eb7d61d
marcovaresi <74302073+marcovaresi@users.noreply.github.com>
2020-12-27 17:41:05 +0100

    Add files via upload

--- stato_mappa.py
+++ stato_mappa.py
@@ -6,13 +6,13 @@ def stato_mappa(mapp, symbol, size, sizex):
 			if mapp[i][j] == '#' or mapp[i][j] == '@' or mapp[i][j] == '&':
 				stato[i][j] = 1
 			if (mapp[i][j] == 'X' and symbol == 0) or (mapp[i][j] == 'x' and symbol == 1):
-				stato[i][j] = 100
+				stato[i][j] = 300
 	stallo = True
 	while stallo:
 		stallo = False
 		for i in range(1, size-1):
 			for j in range(1, sizex-1):
-				if stato [i][j] != 1 and stato[i][j] != 100:
+				if stato [i][j] != 1 and stato[i][j] != 300:
 					t = stato[i][j]
 					stato[i][j] = max(stato[i+1][j], stato[i-1][j], stato[i][j+1], stato[i][j-1]) - 1 
 					if stato[i][j] == -1 or stato[i][j] == t:
@@ -25,8 +25,8 @@ def stato_mappa(mapp, symbol, size, sizex):
 		if stato[size-1][i] == 0:
 			stato[size-1][i] = stato[size-2][i]- 1
 	for i in range(0, size):
-		if stato[i][0] == 0:
+		if stato[i][0] == 0 or stato[i][0] == -1:
 			stato[i][0] = stato[i][1] - 1
-		if stato[i][size-1] == 0:
+		if stato[i][size-1] == 0 or stato[i][size-1] == -1:
 			stato[i][sizex-1] = stato[i][sizex-2] - 1
 	return stato
